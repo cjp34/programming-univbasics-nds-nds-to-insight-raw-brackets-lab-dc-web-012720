8650f8de50a53d26fd2ad011182438c0ea4d2445
Carl Parm <carlparm@gmail.com>
2020-01-22 16:49:49 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,7 +2,16 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
-  pp nds   
+  directors = 0
+  while directors < nds.length do
+    
+  
+  
+  
+    puts directors_database[0][:movies][count][:title]
+    count += 1
+    
+    
    # result = {
  # }
 end
