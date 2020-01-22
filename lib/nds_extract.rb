e74072c6ce1a5162fa0b36091496d9c4139cef65
Carl Parm <carlparm@gmail.com>
2020-01-22 17:54:23 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -3,12 +3,12 @@ require 'directors_database'
 require 'pry'
 
 def directors_totals(nds)
-  total = 0
   directors = 0
   result = {}
   while directors < nds.length do
     result[nds[directors][:name]] = 0
     movie = 0
+    total = 0
     while movie < nds[directors][:movies].length do
       total += nds[directors][:movies][movie][:worldwide_gross]
       movie += 1
