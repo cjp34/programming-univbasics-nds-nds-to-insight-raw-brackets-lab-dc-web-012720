c146bb328a60a8f8ae1dc2d4268c2e3930169ad3
Carl Parm <carlparm@gmail.com>
2020-01-22 16:45:52 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,11 +2,7 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
-    
-    
-    
-    result = {
-      :name
-  }
-  nil
+  pp nds   
+   # result = {
+ # }
 end
